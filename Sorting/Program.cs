768dea2065aad06c3d82cc565cab1675a77745fa
Maneh Harutyunyan <maneh.harutyunyan@gmail.com>
2019-04-05 20:41:02 +0400

    kk

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -13,6 +13,7 @@ namespace Sorting
         {
             for (int i = 0; i < arr.Length;  i++)
             {
+                Console.WriteLine();
                 Console.Write(arr[i] + " ");
             }
             Console.WriteLine();
